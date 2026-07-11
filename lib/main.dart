6b198a00316f011e2da88a6f767408ab5c4fa9c4
anglebert dev <anglebertsh@gmail.com>
2026-07-11 16:15:31 +0200

    Integrate Firebase configuration and Firestore rules; add GoogleService-Info.plist and google-services.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/routes/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
